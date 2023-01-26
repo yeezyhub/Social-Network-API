50f512a5306068cbb16a8f8a615f2bef4a0f720c
Yigit Ocak <yigitocak@Yigits-MacBook-Pro.local>
2023-01-26 14:48:15 -0600

    User.js model is created.

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+// Schemas define the shape of the documents within the collection.
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (validation) {
+        return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(validation);
+      },
+      message: (props) => `${props.value} is not a valid email address!`,
+    },
+  },
+
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Student',
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+
+  toJSON: {
+    virtuals: true,
+    getters: true,
+  },
+  id: false,
+
+});
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
